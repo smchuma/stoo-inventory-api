381f34220c040c75fb845f6bc520f9d750c5adae
Samora Mchuma <laurentsamora6@gmail.com>
2024-12-28 14:49:49 +0300

    create the authcontroller and implement sanctum tokens

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    //
+
+    public function register(Request $request){
+
+        $fields = $request->validate([
+            "first_name"=> "required|string|max:255",
+            "last_name"=> "required|string|max:255",
+            "email"=> "required|unique:suppliers,email",
+            "password"=> "required",
+           'phone_number' => [
+                'required',
+                'regex:/^0[67]\d{8}$/',
+                'unique:suppliers,phone_number'
+                ],
+        ]);
+
+        $user = User::create($fields);
+        $token = $user->createToken($request->first_name);
+
+        return [
+            'token'=> $token->plainTextToken,
+            'user'=> $user,
+        ];
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+           "email"=> "required|email|exists:users",
+           "password"=> "required"
+       ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+           return [
+               'message' => "The credentials provided are incorrect"
+           ];
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            "user" => $user,
+            "token"=> $token->plainTextToken
+
+        ];
+
+
+
+   }
+   public function logout(Request $request){
+
+       $request->user()->tokens()->delete();
+
+       return [
+           'message' => "you are logged out"
+       ];
+   }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
@@ -11,9 +12,11 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 
+Route::post("/register", [AuthController::class,"register"]);
+Route::post("/login", [AuthController::class,"login"]);
+Route::post("/logout", [AuthController::class,"logout"]);
+
 
 Route::apiResource("/category",CategoryController::class);
-
 Route::apiResource('/product', ProductController::class);
-
 Route::apiResource('/supplier', SupplierController::class);
